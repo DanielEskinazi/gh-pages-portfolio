345d7243d3439a70530b6760a2ff5fa82767abd7
DanielEskinazi <deskinazi11@gmail.com>
2024-11-20 15:42:57 -0500

    add header component with scroll functionality and update styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Header.css"; // Import CSS for styling
 
-const Header = () => (
-  <header>
-    <nav>
-      <a href="#about">About</a>
-      <a href="#projects">Projects</a>
-      <a href="#skills">Skills</a>
-      <a href="#contact">Contact</a>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const [fixedHeader, setFixedHeader] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > window.innerHeight) {
+        setFixedHeader(true);
+      } else {
+        setFixedHeader(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <header className={fixedHeader ? "fixed-header" : ""}>
+      <nav>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#skills">Skills</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,20 +2,24 @@ import React, { useEffect, useState } from "react";
 import "./Intro.css"; // Import CSS for styling
 import Particles from "react-tsparticles"; // Import Particles from react-tsparticles
 import { loadFull } from "tsparticles"; // Import loadFull from tsparticles
+import { loadLinksPreset } from "tsparticles-preset-links"; // Import Links preset
 
 const Intro = () => {
   const [showParticles, setShowParticles] = useState(true);
+  const [fixedHeader, setFixedHeader] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > window.innerHeight) {
-        document.body.classList.remove("hide-footer", "hide-header");
-        document.body.classList.add("show-footer", "show-header");
+      if (window.scrollY > 0) {
         setShowParticles(false);
       } else {
-        document.body.classList.add("hide-footer", "hide-header");
-        document.body.classList.remove("show-footer", "show-header");
         setShowParticles(true);
+      }
+
+      if (window.scrollY > window.innerHeight) {
+        setFixedHeader(true);
+      } else {
+        setFixedHeader(false);
       }
     };
 
@@ -29,54 +33,39 @@ const Intro = () => {
 
   const particlesInit = async (main) => {
     await loadFull(main);
+    await loadLinksPreset(main);
   };
 
   return (
-    <section id="intro" className="full-page">
-      {showParticles && (
-        <Particles
-          id="tsparticles"
-          init={particlesInit}
-          options={{
-            background: {
-              color: {
-                value: "#ffffff",
+    <>
+      <section id="intro" className="full-page">
+        {showParticles && (
+          <Particles
+            id="tsparticles"
+            init={particlesInit}
+            options={{
+              preset: "links",
+              background: {
+                color: {
+                  value: "#252a34", // Update background color
+                },
               },
-            },
-            particles: {
-              number: {
-                value: 100,
-              },
-              size: {
-                value: 5,
-              },
-              color: {
-                value: "#007bff",
-              },
-              links: {
-                enable: true,
-                color: "#007bff",
-                distance: 150,
-              },
-              move: {
-                enable: true,
-                speed: 2,
-              },
-            },
-          }}
-          className="particles"
-        />
-      )}
-      <div className="centered-content">
-        <h2>About Me</h2>
-        <p className="text">
-          Hello, I'm Daniel Eskinazi. I'm a full-stack developer.
-        </p>
-        <a href="#about" className="button smooth-scroll">
-          Learn More
-        </a>
-      </div>
-    </section>
+            }}
+            className="particles"
+          />
+        )}
+        <div className="centered-content">
+          <p className="text">
+            Hello, I'm <span id="my-name">Daniel Eskinazi.</span>
+            <br />
+            I'm a full-stack developer.
+          </p>
+          <a href="#about" className="button smooth-scroll">
+            Learn More
+          </a>
+        </div>
+      </section>
+    </>
   );
 };
 
